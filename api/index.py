dfe0fd9cf72e05183657a48a09641343f26d3bb4
Stephan <stephanhagmann@outlook.com>
2024-05-24 10:59:30 +0200

    Implemented REST-Endpoints for flight

--- api/index.py
+++ api/index.py
@@ -2,10 +2,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+# Return list of every flight
+@app.route('/flights', methods=['GET'])
+def get_flights():
+    return 'Not yet available'
+
+
+# Get specific flight by id
+@app.route('/flights/<id>', methods=['GET'])
+def get_flight(id):
+    return 'Not yet available'
+
+
+# Create a new flight
+@app.route('/flights', methods=['POST'])
+def post_flight():
+    return 'Not yet available'
+
+
+# Replace an already existing flight
+@app.route('/flights', methods=['PUT'])
+def put_flight():
+    return 'Not yet available'
+
+
+# Modify an already existing flight
+@app.route('/flights/<id>', methods=['PATCH'])
+def patch_flight(id):
+    return 'Not yet available'
+
+
+# Delete a flight
+@app.route('/flights/<id>', methods=['DELETE'])
+def patch_flight(id):
+    return 'Not yet available'
