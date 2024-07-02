b55330cf3d9366b9e84001634c4f985baa643f13
Gian Jaya <70436710+GianJaya@users.noreply.github.com>
2024-07-02 13:23:25 +0200

    test update

--- tests/test_flight.py
+++ tests/test_flight.py
@@ -3,14 +3,14 @@
 from api.models.Flight import Flight
 
 def test_flight_initialization():
-    flight = Flight(1, 'New York', 'London')
+    flight = Flight(1, 'New York', 'London', 1)
     assert flight.idFlight == 1
     assert flight.Employee_idEmployee == 1
     assert flight.origin == 'New York'
     assert flight.destination == 'London'
 
 def test_to_dict():
-    flight = Flight(2, 'Paris', 'Tokyo')
+    flight = Flight(2, 'Paris', 'Tokyo', 2)
     flight_dict = flight.to_dict()
     expected_dict = {
         'idFlight': 2,
